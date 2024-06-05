805d554b33bc1555b77876ee54e29fac3a78cdf5
Pong <pongpong34756@gmail.com>
2024-06-05 17:20:21 +0700

    feat:state HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [sayHi, setGreeting] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{sayHi}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button
+          onClick={() => {
+            setGreeting("สวัสดี");
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setGreeting("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setGreeting("你好!");
+          }}
+        >
+          你好!
+        </button>
       </div>
     </div>
   );
